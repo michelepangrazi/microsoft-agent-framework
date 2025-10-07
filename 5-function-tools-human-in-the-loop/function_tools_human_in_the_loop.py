8462cddfb6bae1ec986588b896d1b3001b537f5e
Pangra12 <Pangra12@posteitaliane.it>
2025-10-07 11:28:59 +0200

    docs: add some links

---
+++ 5-function-tools-human-in-the-loop/function_tools_human_in_the_loop.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+from pydantic import Field
+from agent_framework import ai_function
+
+@ai_function(name="weather_tool", description="Retrieves weather information for any location")
+def get_weather(
+    location: Annotated[str, Field(description="The location to get the weather for.")],
+) -> str:
+    return f"The weather in {location} is cloudy with a high of 15°C."
+
+
+import asyncio, os
+from agent_framework.azure import AzureOpenAIChatClient
+
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.environ.get("AZURE_OPENAI_API_KEY")
+API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION")
+ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT")
+DEPLOYMENT_NAME = os.environ.get("AZURE_OPENAI_MODEL")
+
+agent = AzureOpenAIChatClient(
+        api_key=API_KEY,
+        deployment_name=DEPLOYMENT_NAME,
+        api_version=API_VERSION,
+        endpoint=ENDPOINT,
+    ).create_agent(
+        instructions="You are a helpful assistant",
+        tools=get_weather,
+    )
+    
+async def main():
+    response = await agent.run("What is the weather like in Amsterdam?")
+    print(response.text)
+
+asyncio.run(main())
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Microsoft Agent Framework Tutorial
 
+https://learn.microsoft.com/en-us/agent-framework/overview/agent-framework-overview
+
 ### Link
+https://learn.microsoft.com/en-us/agent-framework/overview/agent-framework-overview
 https://github.com/microsoft/agent-framework
